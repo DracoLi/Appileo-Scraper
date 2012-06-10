7fab13378876535689ab46f34dd20143c278fe03
henig <henig@hyperhlabs.com>
2012-06-10 01:01:28 +0000

    Refactored models to support Embedded doc. See README.txt in models

--- app/models/legal.rb
+++
@@ -1,9 +0,0 @@
-class Legal
-  include MongoMapper::EmbeddedDocument
-  
-  key :rights,      String
-  key :agreement,   String
-  belongs_to :app
-  timestamps! 
-  
-end
--- test/unit/store_test.rb
+++ test/unit/store_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class VersionTest < ActiveSupport::TestCase
+class StoreTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
